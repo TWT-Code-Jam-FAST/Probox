c50c1542f65c0faf1d45a369b06a1621a91b1c80
SnowballSH <derek070211@gmail.com>
2021-01-06 17:24:44 -0600

    actually get user's time

--- frontend/pages/api/time.ts
+++ frontend/pages/api/time.ts
@@ -1,9 +1,19 @@
 // for reference: https://nextjs.org/docs/api-routes/introduction
 
+const url = require("url");
+import moment from "moment-timezone";
+
 export default (req, res) => {
-    res.statusCode = 200
+    res.statusCode = 200;
 
-    const crrTime: Date = new Date()
+    const crrTime: Date = new Date();
 
-    res.json({timezone: "locale", time: crrTime.toDateString()})
+    let q = url.parse(req.url, true).query;
+    q = q.timezone && q.timezone.split("/").length == 2 ?
+        q.timezone :
+        moment.tz.guess(true);
+
+    const r = moment.tz(crrTime.toString(), q).format().split("T")[0];
+
+    res.json({timezone: q.toString(), time: r.toString()});
 }
