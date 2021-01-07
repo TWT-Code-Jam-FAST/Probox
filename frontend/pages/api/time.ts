70caaf895fc3c8334d4e167635bfda22599aa899
SnowballSH <derek070211@gmail.com>
2021-01-06 20:36:23 -0600

    fix

--- frontend/pages/api/time.ts
+++ frontend/pages/api/time.ts
@@ -9,12 +9,25 @@ export default (req, res) => {
     const crrTime: Date = new Date();
 
     const q: string = url.parse(req.url, true).query.timezone;
-    const m: string = q && q.split("/").length == 2 ?
+    const m: string = q !== undefined && q.split("/").length == 2 ?
         q :
         moment.tz.guess(true);
 
-    const [r, h] = moment.tz(crrTime.toString(), m).format().split("T");
-    const [x, y] = h.split("+")
+    const [r, h]: string[] = moment.tz(crrTime.toString(), m).format().split("T");
+    const cache: string[] = h.split("+")
+    let x: string = cache[0];
+    let y: string | undefined = cache[1];
+    if (y === undefined) {
+        let cache_ = x.split("-")
+        x = cache_[0]
+        y = "-" + cache_[1]
+    } else {
+        y = "+" + y
+    }
 
-    res.json({timezone: q.toString(), day: r.toString(), time: x.toString(), offset: y.toString()});
+    if (y === undefined) {
+        y = "00:00"
+    }
+
+    res.json({timezone: m.toString(), day: r.toString(), time: x.toString(), offset: y.toString()});
 }
