ea3ba770aa8c7dfcbebc4f19104462ca73bd791d
SnowballSH <derek070211@gmail.com>
2021-01-06 19:42:03 -0600

    menu bar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,17 +1,54 @@
 import React from "react";
-import {AppBar, Toolbar, IconButton, Typography} from "@material-ui/core";
+import router from "next/router";
+
+import {AppBar, Toolbar, IconButton, Typography, Menu, MenuItem} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 
 import Time from "./time";
 
 export default function Navbar(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className="navbar root">
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton edge="start" className="navbar menuButton" color="inherit" aria-label="menu">
+          <IconButton edge="start" className="navbar menuButton"
+                      color="inherit" aria-label="menu"
+                      onClick={handleClick}>
             <MenuIcon/>
           </IconButton>
+
+          <Menu
+            id="navbar-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+
+            {
+              // all menu items
+              [
+                ["Time", "/api/time"], ["Tool 12345", null]
+              ].map(([i, f]) =>
+                <MenuItem
+                  onClick={f ? () => router.push(f) : handleClose}
+                >
+                  {i}
+                </MenuItem>)
+            }
+
+          </Menu>
+
           <Typography variant="h6" className="navbar title">
             {props.title}
           </Typography>
