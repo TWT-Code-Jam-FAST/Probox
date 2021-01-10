ca884a800468ecc84873df091dffbabae2ec5c00
SnowballSH <derek070211@gmail.com>
2021-01-10 10:44:09 -0600

    time

--- frontend/components/navbarTime.jsx
+++ frontend/components/navbarTime.jsx
@@ -1,17 +1,27 @@
 import React, {Component} from "react";
 import {Typography} from "@material-ui/core";
 
+
 class NavbarTime extends Component {
   constructor(props) {
     super(props);
-    this.state = {data: {}};
+    this.interval = undefined;
+    this.state = {time: new Date().toLocaleString()};
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({time: new Date().toLocaleString()}), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
     return (
       <>
         <Typography edge="end" variant="h6" className={this.props.className}>
-          {new Date().toLocaleString()}
+          {this.state.time}
         </Typography>
       </>
     );
