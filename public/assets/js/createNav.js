6e862b947d34377fbe0e5a743b75182af4356f5c
SnowballSH <derek070211@gmail.com>
2021-01-14 10:19:28 -0600

    navbar

---
+++ public/assets/js/createNav.js
@@ -0,0 +1,26 @@
+class Navbar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean" style="background: rgb(255,191,132);">
+        <div class="container"><a class="navbar-brand" href="#">Probox</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link active" href="#">First Item</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Second Item</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Third Item</a></li>
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Dropdown </a>
+                        <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    `;
+  }
+}
+
+customElements.define("my-navbar", Navbar);
