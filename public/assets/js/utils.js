c3da77f4937c4bb1beb6ad8dee405c7ce689ffc5
SnowballSH <derek070211@gmail.com>
2021-01-14 16:43:40 -0600

    max

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -1,19 +1,28 @@
-function searchSO() {
+function searchSO(amount = 10, cache = 0) {
   const oVal = document.getElementById("inputSO").value;
   const val = encodeURIComponent(oVal);
   fetch(
-    `https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&accepted=True&body=${val}&site=stackoverflow`
+    `https://api.stackexchange.com/2.2/search/advanced?pagesize=${amount}&order=desc&sort=activity&accepted=True&q=${val}&site=stackoverflow`
   ).then((v) =>
     v.json().then((data) => {
-      if (data.items.length == 0) {
+      if (data.items.length === 0) {
         document.getElementById("Card-Body").innerHTML =
           "No Results found, please use more concise keywords!";
         return;
       }
-      
-      x = data.items
-        .slice(0, 10)
-        .map((a) => `<a href="${a.link}">${a.title}</a>`);
+
+      if (amount >= 100 || cache === data.items.length) {
+        document.getElementById("Load-More").innerHTML =
+          "<h5>Sorry, no more results</h5>";
+      } else {
+        document.getElementById("Load-More").innerHTML = `
+      <button class="btn btn-primary" type="button" onClick="searchSO(${
+        amount + 10
+      }, ${data.items.length})">Load More</button>`;
+      }
+
+      const d = data.items;
+      const x = d.map((a) => `<a href="${a.link}">${a.title}</a>`);
       document.getElementById(
         "Card-Title"
       ).innerHTML = `Search Result for ${oVal}`;
