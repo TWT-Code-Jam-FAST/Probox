1b4fc40ab47bc8ad073a5aee1a3d36d485f6b61e
Aaron <baokou66@gmail.com>
2021-01-17 20:36:56 -0500

    created the tab transition effect

--- public/assets/js/switchTabs.js
+++ public/assets/js/switchTabs.js
@@ -7,7 +7,7 @@ document.getElementById("home-tab").style.display = "block";
 
 function openTabs(e, tabName) {
   let transitionAnimation =
-    ".transition-1 {position: fixed;top: 0;left: 0;right: 0;bottom: 0;z-index: 99999999999999999;background-color: #212121;opacity: 0;pointer-events: none;transition: 0.5s ease-out;}  .transition-1.is-active {pointer-events: all;opacity: 1;}";
+    ".transition-1 {position: fixed; top: 0; right: -100%; bottom: 0; width:100%;z-index: 99999999999999999;background: linear-gradient(270deg, #FEAE79 0%, rgba(255, 151, 134, 0.979681) 15.44%, #FCB484 100%);transition: 0.5s ease-out;} .transition-1.is-active {right: 0px;} .transition-2 {position: fixed;top: 0;left: -100%;width: 100%;bottom: 0;z-index: 9999999;background: linear-gradient(270deg, #FEAE79 0%, rgba(255, 151, 134, 0.979681) 15.44%, #FCB484 100%);transition: 0.5s ease-out;} .transition-2.is-active {left: 0px;} .transition-3 {position: fixed;top: 100%;left: 0;width: 100%;height: 100%;z-index: 9999999;background: linear-gradient(270deg, #FEAE79 0%, rgba(255, 151, 134, 0.979681) 15.44%, #FCB484 100%);transition: 0.5s ease-out;} .transition-3.is-active {top: 0;} .transition-4 {position: fixed;bottom: 100%;left: 0;width: 100%;height: 100%;z-index: 9999999;background: linear-gradient(270deg, #FEAE79 0%, rgba(255, 151, 134, 0.979681) 15.44%, #FCB484 100%);transition: 0.5s ease-out;} .transition-4.is-active {bottom: 0;}";
   transitionAnimation = document.createTextNode(transitionAnimation);
   document.getElementsByTagName("style")[0].appendChild(transitionAnimation);
 
@@ -27,7 +27,7 @@ function openTabs(e, tabName) {
   let currentTab = document.getElementById(tabName);
   const transition_elements = document.querySelectorAll(".transition");
 
-  console.log(currentTab.children[0].classList.add("is-active"));
+  currentTab.children[0].classList.add("is-active");
   setTimeout(() => {
     for (let transition_el of transition_elements) {
       transition_el.classList.remove("is-active");
