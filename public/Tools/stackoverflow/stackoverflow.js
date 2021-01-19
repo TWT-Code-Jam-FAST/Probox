2afa7a139aa01f61cfbb601619490024d7569c34
Aaron <baokou66@gmail.com>
2021-01-19 13:10:37 -0500

    got the questions displaying working in stackoverflow

---
+++ public/Tools/stackoverflow/stackoverflow.js
@@ -0,0 +1,41 @@
+document.addEventListener("keyup", function (e) {
+  if (e.code == 13 || e.key == "Enter") {
+    let amount = 10;
+    let cache = 0;
+
+    let oVal = document.getElementById("stackoverflow-search-bar");
+    oVal = oVal.value;
+    const val = encodeURIComponent(oVal);
+    fetch(
+      `https://api.stackexchange.com/2.2/search/advanced?pagesize=${amount}&order=desc&sort=activity&accepted=True&q=${val}&site=stackoverflow`
+    ).then((v) =>
+      v.json().then((data) => {
+        if (data.items.length === 0) {
+          document.getElementById("answers-container").innerHTML =
+            "No Results found, please use more concise keywords!";
+          return;
+        }
+
+        if (amount >= 100 || cache === data.items.length) {
+          document.getElementById("Load-More").innerHTML =
+            "<h5>Sorry, no more results</h5>";
+        } else {
+          document.getElementById(
+            "Load-More"
+          ).innerHTML = `<button type="button" onClick="searchSO(${
+            amount + 10
+          }, ${data.items.length})">Load More</button>`;
+        }
+
+        const d = data.items;
+        const x = d.map(
+          (a) => `<a href="${a.link}" target=”_blank">${a.title}</a>`
+        );
+        document.getElementById(
+          "search-results"
+        ).innerHTML = `Search Result for ${oVal}`;
+        document.getElementById("answers-container").innerHTML = x.join("<br>");
+      })
+    );
+  }
+});
