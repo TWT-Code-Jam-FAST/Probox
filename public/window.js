916a20ee1446039bdc6983b0b017ee069cd7dd1b
Aaron <baokou66@gmail.com>
2021-01-18 22:41:03 -0500

    made the iframe work

--- public/window.js
+++ public/window.js
@@ -8,13 +8,21 @@ function create(htmlString) {
   return frag;
 }
 
+function closeWindow(element) {
+  element.parentNode.remove();
+  unblurBackgroundStyle = document.createTextNode(
+    "main { filter: blur(0px);-webkit-filter:blur(0px);} "
+  );
+  document.getElementsByTagName("style")[0].append(unblurBackgroundStyle);
+}
+
 function createStackOverFlow() {
   container = document.getElementById("windows-container");
   stackoverflowPage = create(
-    `<div class="window-container stackoverflow"><iframe src="Tools/stackoverflow/index.html"></iframe></div>;`
+    `<div class="window-container stackoverflow"><button id="window-closer" onclick="closeWindow(this)">×</button><iframe id="stackoverflow-window" src="Tools/stackoverflow/index.html"></iframe></div>`
   );
   blurBackgroundStyle = document.createTextNode(
-    "main {  filter: blur(8px);-webkit-filter: blur(8px);}"
+    "main {  filter: blur(8px);-webkit-filter: blur(8px);} iframe {border: 10px solid #fcdc5c; border-radius: 20px;}"
   );
   container.appendChild(stackoverflowPage);
   document.getElementsByTagName("style")[0].append(blurBackgroundStyle);
