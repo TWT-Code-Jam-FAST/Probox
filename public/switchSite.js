ca4eb1089e9523e475e43dfcef116fcd01ead3dd
Aaron <baokou66@gmail.com>
2021-01-22 08:38:45 -0500

    added a if statement to the website

--- public/switchSite.js
+++ public/switchSite.js
@@ -1,3 +1,5 @@
 let page = window.location.href.split("#")[1] || "home";
 
-window.open(`https://probox.vercel.app/Tools/${page}/index.html`, `_blank`);
+if (page != "home") {
+  window.open(`https://probox.vercel.app/Tools/${page}/index.html`, `_blank`);
+}
