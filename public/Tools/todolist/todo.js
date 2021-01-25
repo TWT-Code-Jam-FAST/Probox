4ff513917ab2f78c2241cc06ac2550298f4400a4
Aaron <baokou66@gmail.com>
2021-01-25 10:07:49 -0500

    made the todolist working

--- public/Tools/todolist/todo.js
+++ public/Tools/todolist/todo.js
@@ -2,17 +2,35 @@ var taskBox = document.getElementById("todo-task-box");
 var addTaskButton = document.getElementById("add-task-btn");
 var taskInput = document.getElementById("task-input");
 var createNewButton = document.getElementById("create-task-btn");
+var taskContainer = document.getElementById("tasks-container");
+var taskCount = 0;
+
+function validateTask (string){
+    return string.replace(/\s/g, '').length && string.length
+}
+
+
 
 createNewButton.addEventListener("click", ()=> {
     addTaskButton.style.display = "block";
     taskInput.style.display = "block";
 
     addTaskButton.addEventListener("click", () => {
-        let newTask = document.createElement("li");
-        newTask.innerHTML = taskInput.value;
-        taskBox.appendChild(newTask);
-        taskInput.value = "";
-        taskInput.style.display = "none";
-        addTaskButton.style.display = "none";
-    });
+        if(validateTask(taskInput.value)) {
+            let newTask = document.createElement("li");
+            newTask.innerHTML = taskInput.value;
+            newTask.classList.add("task");
+            taskContainer.style.display = "block";
+            taskCount++;
+            newTask.id = "task"+taskCount; 
+            taskBox.appendChild(newTask);
+            taskInput.value = "";
+            taskInput.style.display = "none";
+            addTaskButton.style.display = "none";
+        } else {
+            taskInput.style.borderColor = "red";
+        }
+
+       });
 });
+
