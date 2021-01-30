28f235363380b05051b6a5751634919f984cabf6
SnowballSH <derek070211@gmail.com>
2021-01-30 13:31:03 -0600

    search

--- public/assets/js/switchTabs.js
+++ public/assets/js/switchTabs.js
@@ -37,3 +37,23 @@ function openTabs(e, tabName) {
     e.currentTarget.className += " opened";
   }
 }
+
+function searchTool() {
+  let input = document.getElementById("tools-searcher").value.trim();
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("list-tools");
+
+  if (input === "") {
+    document.getElementById("list").style.display = "none";
+    return;
+  }
+  document.getElementById("list").style.display = "";
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
