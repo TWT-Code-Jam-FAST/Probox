aae6c0996e50e36be573cb862f46c2dc5ca7ed57
Aaron <baokou66@gmail.com>
2021-01-30 16:39:09 -0500

    made the voice recorder work

---
+++ public/Tools/voiceRecorder/voiceRecorder.js
@@ -0,0 +1,37 @@
+let datas = [];
+let isRecording = false;
+let recordVoiceBtn = document.getElementById("voice-record-btn");
+let audioContainer = document.getElementById("voice-recorder-container");
+let micDevice = navigator.mediaDevices.getUserMedia(
+    {
+        audio:true
+    }
+);
+
+micDevice.then( (stream) => {
+   let voiceRecorder = new MediaRecorder(stream);
+   voiceRecorder.ondataavailable = (evt) => { 
+       datas.push(evt.data);
+       if(voiceRecorder.state == "inactive") {
+           let blob = new Blob(datas,{type:"audio/webm"});
+           let newAudio = document.createElement("audio");
+           newAudio.setAttribute("controls", "controls");
+           newAudio.innerHTML = `<source src="${URL.createObjectURL(blob)}" type="video/webm"/>`;
+           audioContainer.appendChild(newAudio);
+        }
+    };
+    recordVoiceBtn.addEventListener("click", () => {
+       if(isRecording) {
+            voiceRecorder.stop();
+            isRecording = false;
+            recordVoiceBtn.innerHTML = "Record";
+    
+        } else {
+            voiceRecorder.start(100);
+            isRecording = true;
+            recordVoiceBtn.innerHTML = "Stop";
+        }
+        
+        
+    }) 
+});
