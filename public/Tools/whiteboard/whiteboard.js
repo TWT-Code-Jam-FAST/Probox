f11ec1a2affa26878752c6826adec542118335e4
Aaron <baokou66@gmail.com>
2021-01-30 19:13:20 -0500

    made an eraser for the whiteboard

--- public/Tools/whiteboard/whiteboard.js
+++ public/Tools/whiteboard/whiteboard.js
@@ -1,5 +1,13 @@
 var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext("2d");
+var penColor = "black"
+var pencilBtn = document.getElementById("pencil");
+var eraserBtn = document.getElementById("eraser");
+var url;
+
+function drawingCanvasImage(dataUrl, context) {
+}
+
 
 function setWidthAndHeight(){
     canvas.height = window.innerHeight-20;
@@ -14,13 +22,19 @@ window.addEventListener("load", () => {
         if(!isPainting) return;
         ctx.lineWidth = 10;
         ctx.lineCap = "round";
-        ctx.lineTo(evt.clientX, evt.clientY);
+        ctx.strokeStyle = penColor; 
+        ctx.lineTo(evt.clientX, evt.clientY-50);
         ctx.stroke();
     };
     canvas.addEventListener("mousedown",(evt)=>{isPainting = true;paint(evt);});
-    canvas.addEventListener("mouseup",()=>{isPainting = false;ctx.beginPath();});
+    canvas.addEventListener("mouseup",()=>{
+        isPainting = false;
+        ctx.beginPath();
+         
+    });
     canvas.addEventListener("mousemove",paint)
-
+    pencilBtn.addEventListener("click", () => {penColor = "black";});
+    eraserBtn.addEventListener("click", () => {penColor = "white";});
 });
 
 window.addEventListener("resize", () => {
