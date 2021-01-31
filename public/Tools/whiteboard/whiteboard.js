6e9de3ffe96436a6bcdd7e54cbbd3afb7474df6a
SnowballSH <derek070211@gmail.com>
2021-01-30 22:11:56 -0600

    format

--- public/Tools/whiteboard/whiteboard.js
+++ public/Tools/whiteboard/whiteboard.js
@@ -1,42 +1,45 @@
 var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext("2d");
-var penColor = "black"
+var penColor = "black";
 var pencilBtn = document.getElementById("pencil");
 var eraserBtn = document.getElementById("eraser");
 var url;
 
-function drawingCanvasImage(dataUrl, context) {
-}
+function drawingCanvasImage(dataUrl, context) {}
 
-
-function setWidthAndHeight(){
-    canvas.height = window.innerHeight-20;
-    canvas.width = window.innerWidth-20;
+function setWidthAndHeight() {
+  canvas.height = window.innerHeight - 20;
+  canvas.width = window.innerWidth - 20;
 }
 
 window.addEventListener("load", () => {
-    setWidthAndHeight();
-    let isPainting = false;
-    function paint(evt) {
-
-        if(!isPainting) return;
-        ctx.lineWidth = 10;
-        ctx.lineCap = "round";
-        ctx.strokeStyle = penColor; 
-        ctx.lineTo(evt.clientX, evt.clientY-50);
-        ctx.stroke();
-    };
-    canvas.addEventListener("mousedown",(evt)=>{isPainting = true;paint(evt);});
-    canvas.addEventListener("mouseup",()=>{
-        isPainting = false;
-        ctx.beginPath();
-         
-    });
-    canvas.addEventListener("mousemove",paint)
-    pencilBtn.addEventListener("click", () => {penColor = "black";});
-    eraserBtn.addEventListener("click", () => {penColor = "white";});
+  setWidthAndHeight();
+  let isPainting = false;
+  function paint(evt) {
+    if (!isPainting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = penColor;
+    ctx.lineTo(evt.clientX, evt.clientY - 50);
+    ctx.stroke();
+  }
+  canvas.addEventListener("mousedown", (evt) => {
+    isPainting = true;
+    paint(evt);
+  });
+  canvas.addEventListener("mouseup", () => {
+    isPainting = false;
+    ctx.beginPath();
+  });
+  canvas.addEventListener("mousemove", paint);
+  pencilBtn.addEventListener("click", () => {
+    penColor = "black";
+  });
+  eraserBtn.addEventListener("click", () => {
+    penColor = "white";
+  });
 });
 
 window.addEventListener("resize", () => {
-    setWidthAndHeight();
+  setWidthAndHeight();
 });
