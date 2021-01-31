b64b6c0af12c2f4b51872ada5bd61d271b51d1b4
Aaron <baokou66@gmail.com>
2021-01-31 10:11:30 -0500

    save whiteboard to local storage

--- public/Tools/whiteboard/whiteboard.js
+++ public/Tools/whiteboard/whiteboard.js
@@ -3,10 +3,11 @@ var ctx = canvas.getContext("2d");
 var penColor = "black"
 var pencilBtn = document.getElementById("pencil");
 var eraserBtn = document.getElementById("eraser");
-var url;
+var resetBtn = document.getElementById("reset");
+//var whiteBoardWindowCloserBtn = document.getElementById("whiteboard-window-closer"); 
+var dataUrl;
+var penWidth=10;
 
-function drawingCanvasImage(dataUrl, context) {
-}
 
 
 function setWidthAndHeight(){
@@ -15,12 +16,21 @@ function setWidthAndHeight(){
 }
 
 window.addEventListener("load", () => {
+    try {
+        dataUrl = localStorage.getItem("drawing");
+        var dataImg = new Image;
+        dataImg.src = dataUrl;
+        dataImg.onload = () => {
+            ctx.drawImage(dataImg, 0, 0);
+        }
+    }
+    catch {}
     setWidthAndHeight();
     let isPainting = false;
     function paint(evt) {
 
         if(!isPainting) return;
-        ctx.lineWidth = 10;
+        ctx.lineWidth = penWidth;
         ctx.lineCap = "round";
         ctx.strokeStyle = penColor; 
         ctx.lineTo(evt.clientX, evt.clientY-50);
@@ -30,13 +40,27 @@ window.addEventListener("load", () => {
     canvas.addEventListener("mouseup",()=>{
         isPainting = false;
         ctx.beginPath();
-         
+        localStorage.setItem("drawing", canvas.toDataURL());
     });
     canvas.addEventListener("mousemove",paint)
-    pencilBtn.addEventListener("click", () => {penColor = "black";});
-    eraserBtn.addEventListener("click", () => {penColor = "white";});
+    pencilBtn.addEventListener("click", () => {penColor = "black"; penWidth=10;});
+    eraserBtn.addEventListener("click", () => {penColor = "white"; penWidth=15;});
+    resetBtn.addEventListener("click", () => {
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+        localStorage.removeItem("drawing"); 
+    });
 });
 
 window.addEventListener("resize", () => {
+  //  try
+ //       {localStorage.removeItem("drawing");}
+//    catch{}
+    localStorage.setItem("drawing", canvas.toDataURL());
     setWidthAndHeight();
+    dataUrl = localStorage.getItem("drawing");
+    var dataImg = new Image;
+    dataImg.src = dataUrl;
+    dataImg.onload = () => {
+        ctx.drawImage(dataImg, 0, 0);
+    }
 });
